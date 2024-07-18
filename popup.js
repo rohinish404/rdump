5b9df4703bc3b1aa3e1ae71b7d1ec1e48b0f1c62
Rohinish <singhrohinish09@gmail.com>
2024-07-18 18:47:35 +0530

    download txt of urls

--- popup.js
+++ popup.js
@@ -1,6 +1,32 @@
 const dmp_btn = document.getElementById("dmp_btn")
 const all_tabs = document.getElementById("all_tabs")
+function downloadTabs(tabs){
+  let text = tabs.map(tab => tab.url).join('\n')
 
+  let data = new Blob([text], {type: 'text/plain'});
+
+  if (data !== null) {
+    window.URL.revokeObjectURL(data);
+  }
+
+  textFile = window.URL.createObjectURL(data);
+
+  const date = new Date().toISOString().slice(0,10);
+  const options = {
+    url: textFile,
+    filename: `tabs_${date}.txt`,
+  
+  };
+
+
+  chrome.downloads.download(options, (downloadId) => {
+    if (chrome.runtime.lastError) {
+      console.error(chrome.runtime.lastError);
+    } else {
+      console.log('Download initiated with ID:', downloadId);
+    }
+  });
+}
 function dump(){
   all_tabs.innerHTML = ''
   chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
@@ -9,6 +35,11 @@ function dump(){
       elem.innerHTML = `<p>${tab.url}</p>`
       all_tabs.appendChild(elem)
     })
+    downloadTabs(tabs)
   });
 }
+var textFile = null
+
+
+
 dmp_btn.addEventListener('click',dump)
